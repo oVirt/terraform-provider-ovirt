b99c3fb2023c07d2762417a0f227122444e671bc
Ken Schmidt <kenneth.schmidt@pnnl.gov>
2018-03-12 16:59:47 -0700

    Update licensing information

--- main.go
+++ main.go
@@ -1,3 +1,9 @@
+// Copyright (C) 2017 Battelle Memorial Institute
+// All rights reserved.
+//
+// This software may be modified and distributed under the terms
+// of the BSD-2 license.  See the LICENSE file for details.
+
 package main
 
 import (
