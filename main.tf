2761568336126f81177b73d2dfbe9159a293d6a5
imjoey <majunjiev@gmail.com>
2018-05-31 12:26:46 +0800

    define ovirt url/username/password as variable.

--- main.tf
+++ main.tf
@@ -1,41 +1,45 @@
+variable "ovirt_username" {}
+variable "ovirt_url" {}
 variable "ovirt_pass" {}
 
 provider "ovirt" {
-  username = "kschmidt@PNL.GOV"
-  url = "https://ovirt.emsl.pnl.gov/ovirt-engine/api"
+  username = "${var.ovirt_username}"
+  url      = "${var.ovirt_url}"
   password = "${var.ovirt_pass}"
 }
 
-resource "ovirt_vm" "my_vm" {
-  name = "my_first_vm"
-  cluster = "Default"
+resource "ovirt_vm" "joey_vm_1" {
+  name               = "joey_vm_1"
+  cluster            = "Default"
   authorized_ssh_key = "${file(pathexpand("~/.ssh/id_rsa.pub"))}"
+
   network_interface {
-    label = "eth0"
-    boot_proto = "static"
-    ip_address = "130.20.232.184"
-    gateway = "130.20.232.1"
+    label       = "eth0"
+    boot_proto  = "static"
+    ip_address  = "130.20.232.184"
+    gateway     = "130.20.232.1"
     subnet_mask = "255.255.255.0"
   }
 
   attached_disks = [{
-    disk_id = "${ovirt_disk.my_disk.id}"
-    bootable = "false"
-    interface  = "virtio"
+    disk_id   = "${ovirt_disk.joey_disk_1.id}"
+    bootable  = "false"
+    interface = "virtio"
   }]
 
-  template = "centos-7.4.1707-cloudinit-mgmt"
+  template = "Blank"
+
   provisioner "remote-exec" {
     inline = [
-      "uptime"
+      "uptime",
     ]
   }
 }
 
-resource "ovirt_disk" "my_disk" {
-  name = "my_first_disk"
-  size = 1024
-  format = "cow"
-  storage_domain_id = "dfe8e7be-e495-49a7-be2d-71aba891ceb4"
-  sparse = true
+resource "ovirt_disk" "joey_disk_1" {
+  name              = "joey_disk_1"
+  size              = 33687091200
+  format            = "cow"
+  storage_domain_id = "cadbe661-0e35-4fcb-a70d-2b17e2559d9c"
+  sparse            = true
 }
