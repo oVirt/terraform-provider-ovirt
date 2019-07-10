4a22605b6e9097b56ed461d5c49317b066aeff64
imjoey <majunjiev@gmail.com>
2019-07-10 09:12:00 +0800

    Add support for resource ovirt_snapshot
    Signed-off-by: imjoey <majunjiev@gmail.com>

---
+++ ovirt/resource_ovirt_snapshot_test.go
@@ -0,0 +1,114 @@
+package ovirt
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform/helper/resource"
+	"github.com/hashicorp/terraform/terraform"
+	ovirtsdk4 "github.com/ovirt/go-ovirt"
+)
+
+func TestAccOvirtSnapshot_basic(t *testing.T) {
+	description := "description for snapshot"
+	vmID := "53000b15-82ad-4ed4-9f86-bffb95e3c28b"
+	saveMemory := true
+
+	var snapshot ovirtsdk4.Snapshot
+	resource.Test(t, resource.TestCase{
+		PreCheck:      func() { testAccPreCheck(t) },
+		Providers:     testAccProviders,
+		IDRefreshName: "ovirt_snapshot.snapshot",
+		CheckDestroy:  testAccCheckSnapshotDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccSnapshotBasic(description, vmID, saveMemory),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckOvirtSnapshotExists("ovirt_snapshot.snapshot", &snapshot),
+					resource.TestCheckResourceAttr("ovirt_snapshot.snapshot", "description", description),
+					resource.TestCheckResourceAttr("ovirt_snapshot.snapshot", "vm_id", vmID),
+					resource.TestCheckResourceAttr("ovirt_snapshot.snapshot", "save_memory", fmt.Sprintf("%t", saveMemory)),
+					resource.TestCheckResourceAttr("ovirt_snapshot.snapshot", "status", string(ovirtsdk4.SNAPSHOTSTATUS_OK)),
+				),
+			},
+		},
+	})
+}
+
+func testAccCheckSnapshotDestroy(s *terraform.State) error {
+	conn := testAccProvider.Meta().(*ovirtsdk4.Connection)
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "ovirt_snapshot" {
+			continue
+		}
+
+		vmID, snapshotID, err := getVMIDAndSnapshotID(rs.Primary.ID)
+		if err != nil {
+			return err
+		}
+
+		getResp, err := conn.SystemService().
+			VmsService().
+			VmService(vmID).
+			SnapshotsService().
+			SnapshotService(snapshotID).
+			Get().
+			Send()
+
+		if err != nil {
+			if _, ok := err.(*ovirtsdk4.NotFoundError); ok {
+				continue
+			}
+			return err
+		}
+		if _, ok := getResp.Snapshot(); ok {
+			return fmt.Errorf("Snapshot %s still exist", rs.Primary.ID)
+		}
+	}
+	return nil
+}
+
+func testAccCheckOvirtSnapshotExists(n string, v *ovirtsdk4.Snapshot) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[n]
+		if !ok {
+			return fmt.Errorf("Not found: %s", n)
+		}
+		if rs.Primary.ID == "" {
+			return fmt.Errorf("No Snapshot ID is set")
+		}
+
+		vmID, snapshotID, err := getVMIDAndSnapshotID(rs.Primary.ID)
+		if err != nil {
+			return err
+		}
+
+		conn := testAccProvider.Meta().(*ovirtsdk4.Connection)
+		getResp, err := conn.SystemService().
+			VmsService().
+			VmService(vmID).
+			SnapshotsService().
+			SnapshotService(snapshotID).
+			Get().
+			Send()
+		if err != nil {
+			return err
+		}
+		snapshot, ok := getResp.Snapshot()
+		if ok {
+			*v = *snapshot
+			return nil
+		}
+		return fmt.Errorf("Snapshot %s not exist", rs.Primary.ID)
+	}
+}
+
+func testAccSnapshotBasic(description, vmID string, saveMemory bool) string {
+	return fmt.Sprintf(`
+resource "ovirt_snapshot" "snapshot" {
+  description = "%s"
+  vm_id  = "%s"
+  save_memory = %t
+}
+	`, description, vmID, saveMemory)
+}
---
+++ website/docs/r/snapshot.html.markdown
@@ -0,0 +1,46 @@
+---
+layout: "ovirt"
+page_title: "oVirt: ovirt_snapshot"
+sidebar_current: "docs-ovirt-resource-snapshot"
+description: |-
+  Manages a Snapshot of vm resource within oVirt.
+---
+
+# ovirt\_snapshot
+
+Manages a Snapshot of VM resource within oVirt.
+
+## Example Usage
+
+```hcl
+resource "ovirt_snapshot" "snapshot" {
+  description = "description-of-snasphot"
+  vm_id       = "53000b15-82ad-4ed4-9f86-bffb95e3c28b"
+  save_memory = true
+}
+```
+
+## Argument Reference
+
+The following arguments are supported:
+
+* `description` - (Required) A description of the snapshot. Changing this creates a new snapshot.
+* `vm_id` - (Required) The ID of vm the snapshot taken from. Changing this creates a new snapshot.
+* `save_memory` - (Optional) The flag to indicate whether the content of the memory of the vm is included in the snapshot. Default is `true`. Changing this creates a new snapshot.
+
+## Attributes Reference
+
+In addition to all arguments above, the following attributes are exported:
+
+* `id` - The composite ID of the snapshot which is constituted by the ID of the vm the snapshot taken from and the ID of the snapshot within oVirt.
+* `status` - The status of the snapshot. Can be "in_preview", "locked" or "ok".
+* `type` - The type of the snapshot. Can be "active", "preview", "regular" or "stateless".
+* `date` - The string representation of the creation time of the snapshot in RFC3339 format.
+
+## Import
+
+Snapshots can be imported using the composite `id`, e.g.
+
+```
+$ terraform import ovirt_snapshot.snapshot 53000b15-82ad-4ed4-9f86-bffb95e3c28b:df736600-b8be-4029-be98-4b0611be6be4
+```
