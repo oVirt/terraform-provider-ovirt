52e969108e74bf42b8413244bc9fb4865494f48a
Jake Reynolds <jakealexis@gmail.com>
2021-04-30 11:46:55 +0100

    Read VM Nics in resource_ovirt_vm read

--- ovirt/resource_ovirt_vm.go
+++ ovirt/resource_ovirt_vm.go
@@ -1064,7 +1064,7 @@ func resourceOvirtVMRead(d *schema.ResourceData, meta interface{}) error {
 	conn := meta.(*ovirtsdk4.Connection)
 
 	getVmresp, err := conn.SystemService().VmsService().
-		VmService(d.Id()).Get().Send()
+		VmService(d.Id()).Get().Follow("nics").Send()
 	if err != nil {
 		if _, ok := err.(*ovirtsdk4.NotFoundError); ok {
 			d.SetId("")
@@ -1107,6 +1107,17 @@ func resourceOvirtVMRead(d *schema.ResourceData, meta interface{}) error {
 		}
 
 		d.Set("boot_devices", os[0]["boot"].(map[string]interface{})["devices"])
+	}
+
+	if nics, ok := vm.Nics(); ok {
+		readNics := make([]map[string]interface{}, len(nics.Slice()))
+		for index, nic := range nics.Slice() {
+			n := make(map[string]interface{})
+			n["name"] = nic.MustName()
+			n["vnic_profile_id"] = nic.MustVnicProfile().MustId()
+			readNics[index] = n
+		}
+		d.Set("nics", readNics)
 	}
 
 	// If the virtual machine is cloned from a template or another virtual machine,
