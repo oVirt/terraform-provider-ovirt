dded375bf108087efddcbd9a937bcf71937c72c9
Evgeny Slutsky <eslutsky@redhat.com>
2021-06-10 17:56:25 +0300

    fix TF tests: TestAccOvirtVNicProfilesDataSource
    Signed-off-by: Evgeny Slutsky <eslutsky@redhat.com>

--- ovirt/data_source_ovirt_vnic_profiles_test.go
+++ ovirt/data_source_ovirt_vnic_profiles_test.go
@@ -1,6 +1,7 @@
 package ovirt_test
 
 import (
+	"fmt"
 	"regexp"
 	"testing"
 
@@ -8,27 +9,40 @@ import (
 )
 
 func TestAccOvirtVNicProfilesDataSource_nameRegexFilter(t *testing.T) {
+
 	suite := getOvirtTestSuite(t)
+
+	network, err := suite.CreateTestNetwork()
+	if network != nil {
+		defer func() {
+			if err := suite.DeleteTestNetwork(network); err != nil {
+				t.Fatal(fmt.Errorf("failed to delete test network (%w)", err))
+			}
+		}()
+	}
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	networkID := network.MustId()
 	resource.Test(t, resource.TestCase{
 		PreCheck:  suite.PreCheck,
 		Providers: suite.Providers(),
 		Steps: []resource.TestStep{
 			{
-				Config: testAccCheckOvirtVNicProfilesDataSourceNameRegexConfig,
+				Config: fmt.Sprintf(`
+data "ovirt_vnic_profiles" "name_regex_filtered_cluster" {
+  name_regex = "terraform-test.*"
+  network_id = "%s"
+}
+`,networkID),
 				Check: resource.ComposeTestCheckFunc(
 					suite.TestDataSource("data.ovirt_vnic_profiles.name_regex_filtered_cluster"),
-					resource.TestCheckResourceAttr("data.ovirt_vnic_profiles.name_regex_filtered_cluster", "vnic_profiles.#", "2"),
-					resource.TestMatchResourceAttr("data.ovirt_vnic_profiles.name_regex_filtered_cluster", "vnic_profiles.0.name", regexp.MustCompile("^mirror*")),
-					resource.TestMatchResourceAttr("data.ovirt_vnic_profiles.name_regex_filtered_cluster", "vnic_profiles.1.name", regexp.MustCompile("^no_mirror*")),
+					resource.TestCheckResourceAttr("data.ovirt_vnic_profiles.name_regex_filtered_cluster", "vnic_profiles.#", "1"),
+					resource.TestMatchResourceAttr("data.ovirt_vnic_profiles.name_regex_filtered_cluster", "vnic_profiles.0.name", regexp.MustCompile("^terraform-test*")),
 				),
 			},
 		},
 	})
 }
-
-var testAccCheckOvirtVNicProfilesDataSourceNameRegexConfig = `
-data "ovirt_vnic_profiles" "name_regex_filtered_cluster" {
-  name_regex = ".*mirror$"
-  network_id = "649f2d61-7f23-477b-93bd-d55f974d8bc8"
-}
-`
