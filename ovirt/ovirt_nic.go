ce5a8acd2e432b6648924c3aa9514d433c62a751
gzaidman <gzaidman@redhat.com>
2021-10-31 09:06:33 +0200

    Add Update to NIC resource

--- ovirt/ovirt_nic.go
+++ ovirt/ovirt_nic.go
@@ -19,7 +19,6 @@ var nicSchema = map[string]*schema.Schema{
 		Type:             schema.TypeString,
 		Required:         true,
 		Description:      "ID of the VNIC profile to associate with this NIC.",
-		ForceNew:         true,
 		ValidateDiagFunc: validateUUID,
 	},
 	"vm_id": {
@@ -33,7 +32,6 @@ var nicSchema = map[string]*schema.Schema{
 		Type:             schema.TypeString,
 		Required:         true,
 		Description:      "Human-readable name for the NIC.",
-		ForceNew:         true,
 		ValidateDiagFunc: validateNonEmpty,
 	},
 }
@@ -42,6 +40,7 @@ func (p *provider) nicResource() *schema.Resource {
 	return &schema.Resource{
 		CreateContext: p.nicCreate,
 		ReadContext:   p.nicRead,
+		UpdateContext: p.nicUpdate,
 		DeleteContext: p.nicDelete,
 		Importer: &schema.ResourceImporter{
 			StateContext: p.nicImport,
@@ -74,6 +73,58 @@ func (p *provider) nicRead(ctx context.Context, data *schema.ResourceData, _ int
 			return nil
 		}
 		return errorToDiags("get NIC", err)
+	}
+	return nicResourceUpdate(nic, data)
+}
+
+func (p *provider) nicUpdate(ctx context.Context, data *schema.ResourceData, _ interface{}) diag.Diagnostics {
+	params := ovirtclient.UpdateNICParams()
+	var err error
+	if vnicProfileID, ok := data.GetOk("vnic_profile_id"); ok {
+		params, err = params.WithVNICProfileID(vnicProfileID.(string))
+		if err != nil {
+			return diag.Diagnostics{
+				diag.Diagnostic{
+					Severity: diag.Error,
+					Summary:  "Invalid vnic_profile_id value.",
+					Detail:   err.Error(),
+				},
+			}
+		}
+	}
+	if name, ok := data.GetOk("name"); ok {
+		params, err = params.WithName(name.(string))
+		if err != nil {
+			return diag.Diagnostics{
+				diag.Diagnostic{
+					Severity: diag.Error,
+					Summary:  "Invalid name value.",
+					Detail:   err.Error(),
+				},
+			}
+		}
+	}
+	vmID, ok := data.GetOk("vm_id")
+	if !ok {
+		if err != nil {
+			return diag.Diagnostics{
+				diag.Diagnostic{
+					Severity: diag.Error,
+					Summary:  "Invalid name value.",
+					Detail:   err.Error(),
+				},
+			}
+		}
+	}
+	nic, err := p.client.UpdateNIC(vmID.(string), data.Id(), params, ovirtclient.ContextStrategy(ctx))
+	if err != nil {
+		return diag.Diagnostics{
+			diag.Diagnostic{
+				Severity: diag.Error,
+				Summary:  "Failed to update nic.",
+				Detail:   err.Error(),
+			},
+		}
 	}
 	return nicResourceUpdate(nic, data)
 }
