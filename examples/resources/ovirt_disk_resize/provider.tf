168a4f255326e390d1ce9f4236c5975a9d2a22d4
Michael Engel <engel.michael@hotmail.de>
2022-06-07 21:06:09 +0200

    Fixes #344: added ovirt_disk_resize resource (#431)

---
+++ examples/resources/ovirt_disk_resize/provider.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    ovirt = {
+      source  = "ovirt/ovirt"
+    }
+  }
+
+  required_version = ">= 0.15"
+}
+
+provider "ovirt" {
+  url           = var.url
+  username      = var.username
+  password      = var.password
+  tls_ca_bundle = var.tls_ca_bundle
+  tls_system    = var.tls_system
+  tls_ca_dirs   = var.tls_ca_dirs
+  tls_ca_files  = var.tls_ca_files
+  tls_insecure  = var.tls_insecure
+}
---
+++ internal/ovirt/resource_ovirt_disk_resize_test.go
@@ -0,0 +1,55 @@
+package ovirt
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+	ovirtclient "github.com/ovirt/go-ovirt-client"
+)
+
+func TestDiskResizeResource(t *testing.T) {
+	t.Parallel()
+
+	p := newProvider(newTestLogger(t))
+
+	helper := p.getTestHelper()
+	disk, err := helper.GetClient().CreateDisk(
+		helper.GetStorageDomainID(), ovirtclient.ImageFormatRaw, 1048576,
+		ovirtclient.CreateDiskParams().MustWithAlias("TestDisk").MustWithSparse(true))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	resource.UnitTest(t, resource.TestCase{
+		ProviderFactories: p.getProviderFactories(),
+		Steps: []resource.TestStep{
+			{
+				Config: fmt.Sprintf(`
+					provider "ovirt" {
+						mock = true
+					}
+					resource "ovirt_disk_resize" "resized" {
+						disk_id = "%s"
+						size = 2*1048576
+					}`,
+					disk.ID(),
+				),
+				Check: resource.ComposeTestCheckFunc(
+					func(state *terraform.State) error {
+						diskID := state.RootModule().Resources["ovirt_disk_resize.resized"].Primary.ID
+						disk, err := p.getTestHelper().GetClient().GetDisk(ovirtclient.DiskID(diskID))
+						if err != nil {
+							return err
+						}
+						if disk.ProvisionedSize() != 2097152 {
+							return fmt.Errorf("Expected disk size to be 2097152, but got %d", disk.ProvisionedSize())
+						}
+						return nil
+					},
+				),
+			},
+		},
+	})
+}
