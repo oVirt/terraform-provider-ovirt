ab078b3cb4e86545157a351ef89ee8f361f516df
Veronika Fuxova <vfuxova@redhat.com>
2022-06-09 20:16:45 +0200

    Fixes #281: wait for IP

---
+++ internal/ovirt/data_source_ovirt_wait_for_ip_test.go
@@ -0,0 +1,59 @@
+package ovirt
+
+import (
+	"fmt"
+	"regexp"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+)
+
+func TestOvirtWaitForIP(t *testing.T) {
+	t.Parallel()
+
+	p := newProvider(newTestLogger(t))
+	clusterID := p.getTestHelper().GetClusterID()
+	templateID := p.getTestHelper().GetBlankTemplateID()
+	config := fmt.Sprintf(
+		`
+provider "ovirt" {
+	mock = true
+}
+
+resource "ovirt_vm" "foo" {
+	cluster_id = "%s"
+	template_id = "%s"
+	name = "test"
+}
+
+resource "ovirt_vm_start" "foo" {
+	vm_id = ovirt_vm.foo.id
+}
+data "ovirt_wait_for_ip" "test" {
+    vm_id = ovirt_vm.foo.id
+}
+`,
+		clusterID,
+		templateID,
+	)
+
+	resource.UnitTest(t, resource.TestCase{
+		ProviderFactories: p.getProviderFactories(),
+		Steps: []resource.TestStep{
+			{
+				Config: config,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestMatchResourceAttr(
+						"ovirt_vm_start.foo",
+						"status",
+						regexp.MustCompile("up"),
+					),
+				),
+			},
+			{
+				Config:  config,
+				Destroy: true,
+			},
+		},
+	})
+}
